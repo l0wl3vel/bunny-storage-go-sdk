4a1ad76b9bab408edba3e087e821c1b476ea6668
Benjamin Ritter <Benjamin.Ritter@protonmail.com>
2024-01-20 23:27:24 +0100

    feat: add more pre-commit hooks

--- client_test.go
+++ client_test.go
@@ -19,17 +19,17 @@ var bunnyclient bunnystorage.Client
 
 var testingDirectory string
 
-func TestMain(m *testing.M)	{
+func TestMain(m *testing.M) {
 	password = os.Getenv("BUNNY_PASSWORD")
 	endpointString := os.Getenv("BUNNY_ENDPOINT")
-	if password == ""	{
+	if password == "" {
 		panic("No API Key provided (BUNNY_PASSWORD)")
 	}
-	if endpointString == ""	{
+	if endpointString == "" {
 		panic("No Endpoint provided (BUNNY_ENDPOINT)")
 	}
 	endpoint, err := endpoint.Parse(endpointString)
-	if err != nil	{
+	if err != nil {
 		panic(err)
 	}
 
@@ -39,126 +39,126 @@ func TestMain(m *testing.M)	{
 	m.Run()
 }
 
-func DeleteFileWithPathSetToTrue(t *testing.T)	{
-	t.Cleanup(func() {DeleteTestPath(t)})
+func DeleteFileWithPathSetToTrue(t *testing.T) {
+	t.Cleanup(func() { DeleteTestPath(t) })
 	_ = UploadRandomFile1MB(t)
 	testpath := path.Join(testingDirectory, t.Name())
 
 	err := bunnyclient.Delete(testpath, true)
-	if err != nil	{
+	if err != nil {
 		t.Error(err)
 	}
 	list := ListFilesInTestDir(t)
-	if len(list) != 0	{
+	if len(list) != 0 {
 		t.Errorf("Returned List not as long as expected: Got: %v Expected %v", len(list), 1)
 	}
 }
 
-func TestGetNonexistentFile(t *testing.T)	{
+func TestGetNonexistentFile(t *testing.T) {
 	body, err := bunnyclient.Download("thispathdoesnotexist")
-	if err == nil	{
+	if err == nil {
 		t.Error("Error should not be nil when getting a non-existent file")
 	}
-	if body != nil	{
+	if body != nil {
 		t.Error("Returned buffer should be nil when getting a non-existent file")
 	}
 }
 
-func TestDeleteNonexistentFile(t *testing.T)	{
+func TestDeleteNonexistentFile(t *testing.T) {
 	err := bunnyclient.Delete("thispathdoesnotexist", false)
-	if err != nil	{
+	if err != nil {
 		t.Error("Expected no error when deleting a file that does not exist")
 	}
 }
 
-func TestDownloadAfterUpload1M(t *testing.T)	{
-	t.Cleanup(func() {DeleteTestPath(t)})
+func TestDownloadAfterUpload1M(t *testing.T) {
+	t.Cleanup(func() { DeleteTestPath(t) })
 	input := UploadRandomFile1MB(t) // 1MB file size
 	output := DownloadFile(t)
 	list := ListFilesInTestDir(t)
-	if len(list) != 1	{
+	if len(list) != 1 {
 		t.Errorf("Returned List not as long as expected: Got: %v Expected %v", len(list), 1)
 	}
 
-	if !reflect.DeepEqual(input, output)	{
+	if !reflect.DeepEqual(input, output) {
 		t.Error("Downloaded Content does not match uploaded content")
 	}
 }
 
-func TestListAfterUploadWithExtraTrailingSlash(t *testing.T)	{
-	t.Cleanup(func() {DeleteTestPath(t)})
+func TestListAfterUploadWithExtraTrailingSlash(t *testing.T) {
+	t.Cleanup(func() { DeleteTestPath(t) })
 	_ = UploadRandomFile1MB(t) // 1MB file size
-	list, err := bunnyclient.List(testingDirectory+"/")
-	if err != nil	{
+	list, err := bunnyclient.List(testingDirectory + "/")
+	if err != nil {
 		t.Error(err)
 	}
-	if len(list) != 1	{
+	if len(list) != 1 {
 		t.Errorf("Returned List not as long as expected: Got: %v Expected %v", len(list), 1)
 	}
 }
 
-func TestListOnMissingDirectory(t *testing.T)	{
-	list, err := bunnyclient.List(testingDirectory+t.Name())
-	if err != nil	{
+func TestListOnMissingDirectory(t *testing.T) {
+	list, err := bunnyclient.List(testingDirectory + t.Name())
+	if err != nil {
 		t.Error(err)
 	}
-	if len(list) != 0	{
+	if len(list) != 0 {
 		t.Errorf("Returned List not as long as expected: Got: %v Expected %v", len(list), 1)
 	}
 }
 
-func ListFilesInTestDir(t *testing.T)	[]bunnystorage.Object	{
+func ListFilesInTestDir(t *testing.T) []bunnystorage.Object {
 	items, err := bunnyclient.List(testingDirectory)
-	if err != nil	{
+	if err != nil {
 		t.Error(err)
 	}
 	return items
 }
 
-func DownloadFile(t *testing.T) []byte	{
+func DownloadFile(t *testing.T) []byte {
 	t.Helper()
 	testpath := path.Join(testingDirectory, t.Name())
 
 	body, err := bunnyclient.Download(testpath)
-	if err != nil	{
+	if err != nil {
 		t.Error(err)
 	}
 	return body
 }
 
-func UploadRandomFile1MB(t *testing.T) []byte	{
+func UploadRandomFile1MB(t *testing.T) []byte {
 	t.Helper()
 	testpath := path.Join(testingDirectory, t.Name())
 	testcontent := make([]byte, 1048576)
 
 	_, err := rand.Read(testcontent)
-	if err != nil 	{
+	if err != nil {
 		t.Error(err)
 	}
 
 	err = bunnyclient.Upload(testpath, testcontent, true)
-	if err != nil 	{
+	if err != nil {
 		t.Error(err)
 	}
 	return testcontent
 }
 
-func DeleteFile(t *testing.T)	{
+func DeleteFile(t *testing.T) {
 	t.Helper()
 	testpath := path.Join(testingDirectory, t.Name())
 
 	err := bunnyclient.Delete(testpath, false)
-	if err != nil	{
+	if err != nil {
 		t.Error(err)
 	}
 }
 
-func DeleteTestPath(t *testing.T)	{
+func DeleteTestPath(t *testing.T) {
 	t.Helper()
 	testpath := path.Join(testingDirectory)
 
 	err := bunnyclient.Delete(testpath, true)
-	if err != nil	{
+	if err != nil {
 		t.Error(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,12 +5,12 @@ go 1.20
 require (
 	github.com/go-resty/resty/v2 v2.11.0
 	github.com/google/uuid v1.5.0
+	github.com/minio/sha256-simd v1.0.1
 	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
 	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
-	github.com/minio/sha256-simd v1.0.1 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 )
